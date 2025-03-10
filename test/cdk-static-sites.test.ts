131e2012cd1774812723e4c01fe7455b902b0005
asdiadi <carltimothyadi@gmail.com>
2025-03-11 06:54:41 +0800

    Initial commit

---
+++ test/cdk-static-sites.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStaticSites from '../lib/cdk-static-sites-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-static-sites-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStaticSites.CdkStaticSitesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
